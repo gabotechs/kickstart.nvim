c3f6c3642f1e2652eeedff46967d62ee841cd925
gabotechs <gb.mt.me@gmail.com>
2025-02-09 11:39:59 +0100

    Better telescope file browswer ordering

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -4,5 +4,13 @@ return {
   config = function()
     -- open file_browser with the path of the current buffer
     vim.keymap.set('n', '<C-h>', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
+    require('telescope').setup {
+      extensions = {
+        file_browser = {
+          grouped = true,
+          sorting_strategy = 'ascending',
+        },
+      },
+    }
   end,
 }
